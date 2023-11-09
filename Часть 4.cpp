fca326fa1fbd9976f5c3fc42543b9113507d4438
Artem292 <60403062+Artem292@users.noreply.github.com>
2023-11-09 13:06:30 +0300

    Add files via upload

---
+++ Часть 4.cpp
@@ -0,0 +1,29 @@
+﻿#include<iostream>
+using namespace std;
+int main()
+{
+	setlocale(0, "");
+	const int size = 10;
+	int arr[size];
+	for (int i = 0; i < size; i++)
+	arr[i] = rand() % 21 - 10;
+	cout << "Массив:";
+	for (int i = 0; i < size; i++)
+		cout << arr[i] << "";
+	cout << endl;
+	cout << "Положительные:";
+	for (int i = 0; i < size; i++)
+	{
+		if (arr[i] > 0)
+			cout << arr[i] << "";
+	}
+	cout << endl;
+	cout << "Отрицательные:";
+	for (int i = 0; i < size; i++)
+	{
+		if (arr[i] < 0)
+			cout << arr[i] << "";
+	}
+	cout << endl;
+	return 0;
+}
